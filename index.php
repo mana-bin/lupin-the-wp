3e8a6d0d39e27e44a42bca9c4361b42dc17d1f8c
Nishio Manabu <m.nishio@entershare.jp>
2017-12-01 20:37:03 +0900

    ダウンロードするファイルをindexに変更

--- index.php
+++ index.php
@@ -17,6 +17,32 @@ class Lupin {
         'プラグインを盗む',  
         'level_0', "lupin", array($this, 'view_html'), '');
     }
+
+    function createZip($plugin_dir, $file){
+      $zip = new ZipArchive();
+      $res = $zip->open($file, ZipArchive::CREATE);
+      if($res){ 
+        $baseLen = mb_strlen($plugin_dir);
+        $iterator = new RecursiveIteratorIterator(
+          new RecursiveDirectoryIterator($plugin_dir, FilesystemIterator::SKIP_DOTS | FilesystemIterator::KEY_AS_PATHNAME | FilesystemIterator::CURRENT_AS_FILEINFO
+          ), RecursiveIteratorIterator::SELF_FIRST
+        );
+
+        $list = array();
+        foreach($iterator as $pathname => $info){
+          $localpath = mb_substr($pathname, $baseLen);
+          if( $info->isFile() ){
+            $zip->addFile($pathname, $localpath);
+          } else {
+            $res = $zip->addEmptyDir($localpath);
+          }
+        }
+
+        $zip->close();
+      } else {
+        return false;
+      }
+    }
     function view_html() {
       $plugins = scandir(WP_PLUGIN_DIR);
       $ignores = array(".", "..", ".DS_Store", "index.php");
@@ -27,9 +53,10 @@ class Lupin {
           <tr>
               <th><?php echo $val; ?></th>
               <td>
-                <form action="<?php echo plugins_url("lupin")."/download.php"; ?>" method="post">
+                <form action="" method="post">
                   <input type="hidden" name="plugin_dir" value="<?php echo WP_PLUGIN_DIR."/".$val; ?>">
                   <input type="hidden" name="plugin" value="<?php echo $val; ?>">
+                  <input type="hidden" name="donwload" value="true">
                   <input type="submit" class="button-primary" value="このプラグインを盗む">
                 </form>
               </td>
@@ -37,5 +64,20 @@ class Lupin {
         <?php endforeach; ?>
       </table>
     <?php }
+
 }
 $Lupin = new Lupin;
+
+if($_POST["donwload"] == "true"){
+  $plugin_dir = $_POST["plugin_dir"];
+  $file_name  = $_POST["plugin"].".zip";
+  $file       = dirname(__FILE__) . "/tmp/" . $file_name;
+  $Lupin->createZip($plugin_dir, $file);
+
+  header('Content-Type: application/zip; name="' . $file_name . '"');
+  header('Content-Disposition: attachment; filename="' . $file_name . '"');
+  header('Content-Length: '.filesize($file));
+  echo file_get_contents($file);
+  unlink($file);
+  exit();
+}
